f619e165e16711074712c218080d474591d192ba
Faruq-Hameed <faruqhameed1@gmail.com>
2023-05-22 13:54:14 +0100

    added vercel.json file

--- src/db/server.js
+++ src/db/server.js
@@ -4,7 +4,7 @@ const MONGODB_URL = process.env.MONGODB_URL
 const startServer = async (app) => {
     mongoose.connect(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
-      console.log("Connected to local database successfully");
+      console.log("Connected to database successfully");
     })
     .catch((err) => {
       console.log("Error starting the server because: " + err.message);
